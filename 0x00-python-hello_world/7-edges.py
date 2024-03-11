548812d337cee4a895019fb022bf387f30933c90
muhmadshaban <moshaban96.eng@gmail.com>
2024-03-11 17:54:40 +0200

    remove

--- 0x00-python-hello_world/7-edges.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-word = "Holberton"
-word_fst_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print("Fst 3 letters: {}".format(word_fst_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
